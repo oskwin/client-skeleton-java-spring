e7067884956b86f1f7322809204384483af5942b
Jens Eliasson <jens.eliasson@gmail.com>
2021-11-29 11:36:35 +0100

    removed unused imports

--- application-skeleton-mqtt/src/main/java/eu/arrowhead/application/skeleton/pubsub/PubSubApplicationInitListener.java
+++ application-skeleton-mqtt/src/main/java/eu/arrowhead/application/skeleton/pubsub/PubSubApplicationInitListener.java
@@ -1,13 +1,5 @@
 package eu.arrowhead.application.skeleton.pubsub;
 
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.CertificateException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
